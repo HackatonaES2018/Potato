3eebc06e2f123293437afc9cb74f6c11a4c38f19
Marques <leonardo.bitencourt@acad.pucrs.br>
2018-11-18 04:49:52 -0200

    desvios de fluxo CHAT implementados

--- controller/TEF.js
+++ controller/TEF.js
@@ -1,46 +1,121 @@
-var axios = require ('axios');
+
+const array = 
+[
+    {"Qual a sua idade?":["de 18 a 30", "de 30 a 40.", "de 40 a 50.", "de 50 a 60."]},
+    {"Ótimo! Agora escolha o seu perfil:":["Aposentado", "Servidor publico", "Pensionista", "Militar", "Assalariado privado", "Autonomo", "Desempregado"]},
+    {"Voce mora com os pais?":["sim", "não"]},
+    {"Voce é casado?":["sim", "não"]},
+    {"Voce possui bens no seu nome?": ["casa", "carro", "participa de clube"]},
+    {"Voce esta negativado no serasa":["sim", "não"]},
+    {"Voce já teve algum investimento?":["sim", "não"]},
+    {"O que aconteceu com ele?":["Deu certo", "Deu errado"]},
+    {"Voce ja contratou linha de credito anteriormente?":["sim", "não"]},
+    {"Qual o motivo?":["Investimento", "Pagamento de dividas"]},
+    {"Consegiu pagar em dia?":["sim", "não"]}
+
+];
+var respostas = [];
 
 TEF = {
+    
+    get: function(nome){
+        return respostas.find(function (obj){
+            return obj.pergunta === nome
+         })
+     },
 
-    teste: async function(pos){
-        var x = await axios.post('sb-api.portocred.com.br/credito-pessoal-demo/v1/propostas',data1);
-        sleep(5000);
-        return x;
+    perguntas: function(perg, resp){
+        let response =
+        {
+            "perguntas":'Pperg',
+            "respostas":'Prespo'
+        }
+        switch(perg){
+            case "Qual a sua idade?":
+            console.log("entoruuuu")
+                if(resp === "de 18 a 30"){
+                    response.perguntas = "Ótimo! Agora escolha o seu perfil:"; 
+                    response.respostas = ["Servidor publico", "Pensionista", "Militar", "Assalariado privado", "Autonomo", "Desempregado"]
+                }else{
+                    response.perguntas  = "Ótimo! Agora escolha o seu perfil:"; 
+                    response.respostas = ["Aposentado", "Servidor publico", "Pensionista", "Militar", "Assalariado privado", "Autonomo", "Desempregado"];
+                }
+            break;
+            case "Ótimo! Agora escolha o seu perfil:":
+            if(resp === "Aposentado"){
+                response.perguntas = "Voce é casado?"; 
+                response.respostas = ["sim","não"];
+            }else if(resp === "desempregado"){
+                response.perguntas = "Voce possui bens no seu nome?"; 
+                response.respostas = ["casa","carro","participa de clube","não"];
+            }else{
+                response.perguntas = "Voce mora com os pais?";
+                response.respostas = ["sim","não"];}
+            break;
+            case "Voce mora com os pais?":
+                response.perguntas = "Voce é casado?";
+                response.respostas = ["sim","não"];
+            break;
+            case "Voce é casado?":
+                if((resp === "não") && (TEF.get("Voce mora com os pais?"))){
+                        response.perguntas = "FIM";
+                        response.respostas = "FIM";  
+                }else{
+                    response.perguntas = "Voce possui bens no seu nome?"; 
+                    response.respostas = ["casa","carro","participa de clube","não"];
+                }
+            break;
+            case "Voce possui bens no seu nome?":
+                response.perguntas = "Voce já teve algum investimento?";
+                response.respostas = "sim", "não"; 
+
+            break;
+            case "Voce já teve algum investimento?":
+                if(resp === "sim"){
+                    response.perguntas = "O que aconteceu com ele?";
+                    response.respostas = "Deu certo", "Deu errado";  
+                }else{
+                    response.perguntas = "Voce ja contratou linha de credito anteriormente?";
+                    response.respostas = "sim", "não";
+                 } 
+
+            break;
+            case "O que aconteceu com ele?":
+                response.perguntas = "Voce ja contratou linha de credito anteriormente?";
+                response.respostas = "sim", "não";  
+            break;
+            case "Voce ja contratou linha de credito anteriormente?":
+                if(resp === "sim"){
+                    response.perguntas = "Qual o motivo?";
+                    response.respostas = "Investimento", "Pagamento de dividas";  
+                }   {
+                response.perguntas = "FIM";
+                response.respostas = "FIM"; }
+
+            break;
+            case "Qual o motivo?":
+                response.perguntas = "Consegiu pagar em dia?";
+                response.respostas = "sim", "não"; 
+            break;
+            case "Consegiu pagar em dia?":
+                response.perguntas = "FIM";
+                response.respostas = "FIM"; 
+            break;
+            default :
+                response.perguntas = "ALGO ERRADO!!!!";
+                response.respostas = "ALGO ERRADO!!!!"; 
+        }
+        console.log(response);
+        return response;
+    },
+
+    respostas: function(x){
+        respostas.push(x);
+    },
+    getResp: function(){
+        return respostas;
     }
 }
-var data1 = {
-    "cliente":
-    {
-        "nome": "NINA FRANCISCA VIANA",
-        "cpf": "03028799782",
-        "data-nascimento": "1994-08-14",
-        "email": "ninafranciscaviana@djapan.com.br",
-        "escolaridade": "SUPERIOR",
-        "estado-civil": "SOLTEIRO",
-        "sexo": "FEMININO",
-        "negativado": "NAO",
-    },
-    "proposta":
-    {
-        "valor-desejado": 1500,
-        "data-primeira-parcela": "2018-12-18",
-        "seguro": "SIM",
-        "produto": "CARNE"
-    },
-    "dados-adicionais":
-     [
-        {
-            "campo": "MOTIVO_EMPRESTIMO",
-            "valor": "PAGAR CONTAS"
-        }
-    ]
- };
-function sleep(miliseconds) {
-    var currentTime = new Date().getTime();
- 
-    while (currentTime + miliseconds >= new Date().getTime()) {
-    }
- }
 
 
 module.exports = TEF;
